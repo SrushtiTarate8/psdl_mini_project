2fbde7bc0559ca537fee7924810a7e454f9d560d
SrushtiTarate8 <srushtitarate83@gmail.com>
2025-04-28 15:43:46 +0530

    result

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ from tkinter import*
 from PIL import Image,ImageTk  #pip install pillow
 from course import CourseClass
 from student import studentClass
+from result import resultClass
 
 class RMS:
     def __init__(self,root):
@@ -19,7 +20,7 @@ class RMS:
 
         btn_course=Button(M_Frame,text="Course",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2",command=self.add_course).place(x=20,y=5,width=200,height=40)
         btn_student=Button(M_Frame,text="Student",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2",command=self.add_student).place(x=240,y=5,width=200,height=40)
-        btn_result=Button(M_Frame,text="Result",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=460,y=5,width=200,height=40)
+        btn_result=Button(M_Frame,text="Result",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2",command=self.add_result).place(x=460,y=5,width=200,height=40)
         btn_view=Button(M_Frame,text="View Student Results",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=680,y=5,width=200,height=40)
         btn_logout=Button(M_Frame,text="Logout",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=900,y=5,width=200,height=40)
         btn_exit=Button(M_Frame,text="Exit",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=1120,y=5,width=200,height=40)
@@ -52,6 +53,10 @@ class RMS:
     def add_student(self):
         self.new_win=Toplevel(self.root)
         self.new_obj=studentClass(self.new_win)
+
+    def add_result(self):
+        self.new_win=Toplevel(self.root)
+        self.new_obj=resultClass(self.new_win)
 
 if __name__=="__main__":
     root=Tk()
---
+++ result.py
@@ -0,0 +1,39 @@
+from tkinter import*
+from PIL import Image,ImageTk  #pip install pillow
+from tkinter import ttk,messagebox
+import sqlite3
+class resultClass:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Student Result Management System")
+        self.root.geometry("1200x480+80+170")
+        self.root.config(bg="white")
+        self.root.focus_force()
+        #===title====
+        title=Label(self.root,text="Add Student Result ", font=("goudy old style",20,"bold"),bg="orange",fg="#262626").place(x=10,y=15,width=1180,height=50)
+        #===widgets========
+        #=======variables=====
+        self.var_roll=StringVar()
+        self.var_name=StringVar()
+        self.var_course=StringVar()
+        self.var_marks=StringVar()
+        self.var_full_marks=StringVar()
+        self.var_roll_list=[]
+        lbl_select=Label(self.root,text="Select Student",font=("goudy old style",20,'bold'),bg='white').place(x=50,y=100)
+        lbl_name=Label(self.root,text="Name",font=("goudy old style",20,'bold'),bg='white').place(x=50,y=160)
+        lbl_course=Label(self.root,text="Course",font=("goudy old style",20,'bold'),bg='white').place(x=50,y=220)
+        lbl_marks=Label(self.root,text="Marks Obtained",font=("goudy old style",20,'bold'),bg='white').place(x=50,y=280)
+        lbl_full_marks=Label(self.root,text="Full Marks",font=("goudy old style",20,'bold'),bg='white').place(x=50,y=340)
+        
+        self.txt_student=ttk.Combobox(self.root,textvariable=self.var_roll,values=self.roll_list,font=("goudy old style",15,'bold'),state='readonly',justify=CENTER)
+        self.txt_student.place(x=480,y=100,width=200)
+        self.txt_student.set("Select")
+       
+
+
+
+
+if __name__=="__main__":
+    root=Tk()
+    obj=resultClass(root)
+    root.mainloop()
